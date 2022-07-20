96044edddae1ab11d943533758b797cdbe62ccae
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-20 11:17:26 +0200

    feat : prefix and postfix

---
+++ 3.operationsOnData/3.3PrefixAndPostfix/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // regular increment or decrement (+1 or -1)
+    int value = 4;
+    value = value + 1; // same as
+    value += 1;
+
+    value = value - 1; // same as
+    value -= 1;
+
+    // postfix increment or decrement (value-- or value++)
+
+    value = 5;
+    cout << "the value is (incrementing): " << value++ << endl; // 5
+    cout << "the value is : " << value << endl;                 // 6
+
+    // prefix increment or decrement (--value or ++value)
+
+    value = 5;
+    cout << "the value is (decremented) : " << --value << endl; // 4
+    cout << "the value is : " << value << endl;                 // 4
+    cout << "the value is (incremented) : " << ++value << endl; // 5
+
+    return 0;
+}
